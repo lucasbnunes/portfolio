f437f17113e49fe1611a7d52fb1b34ab94a683e0
lucasbnunes <lucasbnunes23@gmail.com>
2023-01-08 19:49:08 -0300

    chore: add initial global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <style jsx global>{`
+    html {
+      font-family: ${inter.style.fontFamily};
+    }
+  `}</style>
+    <Component {...pageProps} />
+  </>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#fffeff',
+        phantom: {
+          50: '#f6f7f7',
+          100: '#e2e5e4',
+          200: '#c5cac9',
+          300: '#a1a7a6',
+          400: '#7c8585',
+          500: '#626a6a',
+          600: '#4d5453',
+          700: '#404545',
+          800: '#363939',
+          900: '#313434',
+        },
+      },
+    },
   },
   plugins: [],
 };
