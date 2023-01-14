d5b66651d5bda8ca1025f096cf1fc2caa3d268aa
lucasbnunes <lucasbnunes23@gmail.com>
2023-01-13 22:16:17 -0300

    feat: add grab to scroll

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,13 +1,19 @@
+import ScrollContainer from "react-indiana-drag-scroll";
+
 interface TagsProps {
   items: string[];
 }
 
 export default function Tags({ items }: TagsProps) {
   return (
-    <ul className={`w-full flex items-center gap-4  whitespace-nowrap overflow-auto scrollbar-thin scrollbar-thumb-phantom-100 scrollbar-track-phantom-50 py-4 scrollbar-thumb-rounded-sm scrollbar-rounded-sm}`} >
+    <ScrollContainer
+      component="ul"
+      hideScrollbars={false}
+      className={`w-full py-2 flex items-center gap-4 whitespace-nowrap overflow-auto scrollbar-thin scrollbar-thumb-phantom-100 scrollbar-track-phantom-50 scrollbar-thumb-rounded-sm scrollbar-rounded-sm cursor-grab`} draggingClassName="cursor-grabbing"
+    >
       {items.map((item) => (
         <li key={item} className='bg-phantom-50 px-2.5 py-1.5 text-sm rounded-sm'>{item}</li>
       ))}
-    </ul>
+    </ScrollContainer>
   )
 }
