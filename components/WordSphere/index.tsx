5c62be283d221fd429de238f0825553332c18150
lucasbnunes <lucasbnunes23@gmail.com>
2023-01-13 22:37:09 -0300

    revert: add cursor change when grabbing the sphere
    This reverts commit 90e9bf65ce56561b9203f54c6bb49384528f9b9c.

--- components/WordSphere/index.tsx
+++ components/WordSphere/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Canvas, ThreeEvent } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import resolveConfig from 'tailwindcss/resolveConfig'
 import tailwindConfig from '../../tailwind.config.js'
@@ -13,23 +12,11 @@ interface WordSphereProps {
   count?: number
 }
 
-// Adapted from this awesome snippet https://codesandbox.io/embed/spherical-word-cloud-yup2o
 export function WordSphere({ words, count = 6 }: WordSphereProps) {
   const fullConfig = resolveConfig(tailwindConfig) as any
 
-  const [isGrabbing, setIsGrabbing] = React.useState(false)
-  const handleMouseDown = () => setIsGrabbing(true)
-  const handleMouseUp = () => setIsGrabbing(false)
-
-  // Change the mouse cursor on grab
-  React.useEffect(() => {
-    console.log('a')
-    //document.body.style.cursor = hovered ? 'grab' : 'auto'
-    document.body.style.cursor = isGrabbing ? 'grabbing' : 'grab'
-  }, [isGrabbing])
-
   return (
-    <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 35], fov: 90 }} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+    <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 35], fov: 90 }}>
       <fog attach="fog" args={[fullConfig.theme?.colors?.phantom[400], 0, 80]} />
       <Cloud words={words} count={count} radius={20} />
       <OrbitControls autoRotate autoRotateSpeed={0.5} minDistance={10} maxDistance={35} enablePan={false} />
