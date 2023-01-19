36e410e328d547f1a4dcc29533f1d0b4322ae00c
lucasbnunes <lucasbnunes23@gmail.com>
2023-01-18 22:59:53 -0300

    feat: add project section text

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -13,7 +13,7 @@ export function ProjectsSection({ projects }: ProjectsSectionProps) {
       <h3 className='font-semibold text-3xl before:inline-block before:w-3 before:h-3 before:mr-2.5 before:bg-phantom-400 before:rounded-sm'>
         Projetos
       </h3>
-      <p className='mt-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat velit, soluta praesentium, odio asperiores error aliquid.</p>
+      <p className='mt-2'>Aqui você pode ver alguns dos meus projetos mais recentes e conhecer um pouco mais sobre o que eu gosto de desenvolver. Busco sempre inovar e desafiar minhas habilidades com novas tecnologias.</p>
 
       <ProjectsGallery projects={projects} />
     </section>
